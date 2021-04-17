b030dfba181341aa355e9b298f926badceccee58
TOM <1308892566@qq.com>
2021-04-17 11:13:22 +0800

    第一次提交

---
+++ ResultCode.java
@@ -0,0 +1,10 @@
+package com.atguigu.utils;
+
+import org.omg.PortableInterceptor.SUCCESSFUL;
+import org.springframework.stereotype.Repository;
+
+
+public interface ResultCode {
+    public static Integer SUCCESS = 20000;
+    public static Integer ERROR = 20001;
+}
